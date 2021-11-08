bd80df81b9a1fa030939e7c03eb4b8ed89979d3c
emmanuelkwaa <emmanueladjeikwaa@gmail.com>
2021-11-08 18:14:52 -0500

    updatedScheduleModel

--- Models/Schedule.cs
+++ Models/Schedule.cs
@@ -1,9 +1,15 @@
+using System;
+using HMS.Enums;
+
 namespace HMS.Models
 {
     public class Schedule
     {
         public int Id { get; set; }
         public Doctor Doctor { get; set; }
-        public TYPE Type { get; set; }
+        public Day ScheduleDay { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan ShiftLength { get; set; }
     }
 }
